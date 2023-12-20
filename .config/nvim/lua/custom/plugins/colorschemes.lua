82e9a71e7228f8783e6a617414ba7dad50b74272
GeorgiyKuzora <rafale87@gmail.com>
2023-12-20 23:33:39 +0300

    nvim: catppuccine italics change

--- .config/nvim/lua/custom/plugins/colorschemes.lua
+++ .config/nvim/lua/custom/plugins/colorschemes.lua
@@ -13,9 +13,23 @@ return {
           dark = "mocha",
         },
         dim_inactive = {
-          enabled = true,    -- dims the background color of inactive window
+          enabled = true,          -- dims the background color of inactive window
           shade = "dark",
-          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+          percentage = 0.15,       -- percentage of the shade to apply to the inactive window
+        },
+        styles = {                 -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+          conditionals = {},
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
         },
         color_overrides = {
           mocha = {
