d339ae24113a89984d8e38afd1cb54b014433ba2
GeorgiyKuzora <rafale87@gmail.com>
2023-12-27 21:48:49 +0300

    nvim: added macros status in lualine

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -4,13 +4,33 @@ return {
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
     name = 'lualine',
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'catppuccin',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
+    opts = {},
+    config = function()
+      local lualine = require 'lualine'
+      local function show_macro_recording()
+        local recording_register = vim.fn.reg_recording()
+        if recording_register == '' then
+          return ''
+        else
+          return 'Recording @' .. recording_register
+        end
+      end
+      lualine.setup {
+        options = {
+          icons_enabled = true,
+          theme = 'catppuccin',
+          component_separators = '|',
+          section_separators = '',
+        },
+        sections = {
+          lualine_b = {
+            {
+              'macro-recording',
+              fmt = show_macro_recording,
+            },
+          },
+        },
+      }
+    end,
   },
 }
