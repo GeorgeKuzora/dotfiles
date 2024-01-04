4dfa2e9eef040b9c58d1ee1931894148a5f9868a
GeorgiyKuzora <rafale87@gmail.com>
2024-01-04 10:14:11 +0300

    nvim: added autocmd configuration from lazyvim

--- .config/nvim/lua/custom/core/config.lua
+++ .config/nvim/lua/custom/core/config.lua
@@ -1,27 +1,16 @@
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
 -- [[Diagnostics config]]
-vim.diagnostic.config({
-    virtual_text = true,
-    -- signs = { active = signs },
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    float = {
-      focused = false,
-      style = "minimal",
-      border = "rounded",
-      source = "always",
-      header = "",
-      prefix = "",
-    },
-  })
+vim.diagnostic.config {
+  virtual_text = true,
+  -- signs = { active = signs },
+  update_in_insert = true,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focused = false,
+    style = 'minimal',
+    border = 'rounded',
+    source = 'always',
+    header = '',
+    prefix = '',
+  },
+}
--- .config/nvim/lua/custom/core/init.lua
+++ .config/nvim/lua/custom/core/init.lua
@@ -1,5 +1,4 @@
-require("custom.core.keymaps")
-require("custom.core.options")
-require("custom.core.config")
-
-
+require 'custom.core.keymaps'
+require 'custom.core.options'
+require 'custom.core.config'
+require 'custom.core.autocmds'
