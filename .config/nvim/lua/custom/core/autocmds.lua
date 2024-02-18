e53524ca7966f3d8a1ac8c5d39b4f3856d95094d
GeorgiyKuzora <rafale87@gmail.com>
2024-02-18 10:27:10 +0300

    nvim: add comments

--- .config/nvim/lua/custom/core/autocmds.lua
+++ .config/nvim/lua/custom/core/autocmds.lua
@@ -5,6 +5,7 @@ local function augroup(name)
   return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
+--  Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
