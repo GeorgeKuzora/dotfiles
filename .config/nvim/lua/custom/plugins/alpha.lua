6e1541a337560ae73b5035b7216aaa8ef01092f6
GeorgiyKuzora <rafale87@gmail.com>
2024-02-27 20:23:43 +0300

    nvim: remove neo-tree plugin

--- .config/nvim/lua/custom/plugins/alpha.lua
+++ .config/nvim/lua/custom/plugins/alpha.lua
@@ -1,50 +1,50 @@
 return {
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.dashboard")
+  'goolord/alpha-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
 
-        dashboard.section.header.val = {
-            "                                                     ",
-            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-            "                                                     ",
-        }
+    dashboard.section.header.val = {
+      '                                                     ',
+      '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+      '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+      '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+      '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+      '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+      '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+      '                                                     ',
+    }
 
-        -- Set menu
-        dashboard.section.buttons.val = {
-            dashboard.button("e", "New File", "<cmd>ene<CR>"),
-            dashboard.button("SPC e", "Toggle file explorer", "<cmd>Neotree toggle position=left reveal=true<cr>"),
-            dashboard.button("SPC ff", "Find Files", "<cmd>Telescope find_files<CR>"),
-            dashboard.button("SPC fg", "Find Words", "<cmd>Telescope live_grep<CR>"),
-            dashboard.button("SPC ?", "Recent Files", "<cmd>Telescope oldfiles<CR>"),
-            dashboard.button("q", "Quit NVIM", "<cmd>qa<CR>"),
-        }
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button('e', 'New File', '<cmd>ene<CR>'),
+      dashboard.button('SPC e', 'Toggle file explorer', '<cmd>Oil<cr>'),
+      dashboard.button('SPC ff', 'Find Files', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('SPC fg', 'Find Words', '<cmd>Telescope live_grep<CR>'),
+      dashboard.button('SPC ?', 'Recent Files', '<cmd>Telescope oldfiles<CR>'),
+      dashboard.button('q', 'Quit NVIM', '<cmd>qa<CR>'),
+    }
 
-        dashboard.section.header.opts.hl = "DashboardHeader"
-        dashboard.section.footer.opts.hl = "DashboardFooter"
+    dashboard.section.header.opts.hl = 'DashboardHeader'
+    dashboard.section.footer.opts.hl = 'DashboardFooter'
 
-        dashboard.config.layout = {
-            { type = "padding", val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) } },
-            dashboard.section.header,
-            { type = "padding", val = 5 },
-            dashboard.section.buttons,
-            { type = "padding", val = 3 },
-            dashboard.section.footer,
-        }
-        dashboard.config.opts.noautocmd = true
+    dashboard.config.layout = {
+      { type = 'padding', val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) } },
+      dashboard.section.header,
+      { type = 'padding', val = 5 },
+      dashboard.section.buttons,
+      { type = 'padding', val = 3 },
+      dashboard.section.footer,
+    }
+    dashboard.config.opts.noautocmd = true
 
-        require 'alpha'.setup(dashboard.config)
+    require('alpha').setup(dashboard.config)
 
-        -- local startify = require('alpha.themes.startify')
-        -- require 'alpha'.setup(startify.config)
+    -- local startify = require('alpha.themes.startify')
+    -- require 'alpha'.setup(startify.config)
 
-        -- -- Disable folding on alpha buffer
-        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-    end
+    -- -- Disable folding on alpha buffer
+    vim.cmd [[autocmd FileType alpha setlocal nofoldenable]]
+  end,
 }
