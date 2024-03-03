9e8eb0e20f5246540cd934570a98f1a300ae47d6
GeorgiyKuzora <rafale87@gmail.com>
2024-03-03 15:55:43 +0300

    nvim: delete nvim-surround plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -82,15 +82,6 @@ return {
   },
 
   {
-    'kylechui/nvim-surround',
-    version = '*', -- Use for stability; omit to use `main` branch for the latest features
-    event = 'VeryLazy',
-    config = function()
-      require('nvim-surround').setup {}
-    end,
-  },
-
-  {
     'hiphish/rainbow-delimiters.nvim',
     name = 'rainbow_delimiters',
     config = function()
