025dfc8db5ccbc8e6fb9dce162ff85a9b1d982be
GeorgiyKuzora <rafale87@gmail.com>
2024-03-04 00:29:15 +0300

    wezterm: set colorscheme to rose-pine

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -302,12 +302,13 @@ config.unix_domains = {
 }
 config.check_for_updates = false
 
-config.color_scheme = "Wez"
+config.color_scheme = "Rosé Pine (base16)"
 config.colors = {
 	tab_bar = {
 		-- The color of the strip that goes along the top of the window
 		-- (does not apply when fancy tab bar is in use)
-		background = "#000000",
+		-- background = "#000000",
+		background = "#191724",
 
 		-- The active tab is the one that has focus in the window
 		active_tab = {
