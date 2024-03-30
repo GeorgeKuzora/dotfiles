d563ecd30f7e875eea4da7785bcea3ff6d3a1e9c
GeorgiyKuzora <rafale87@gmail.com>
2024-03-30 21:12:09 +0300

    nvim + wezterm: add nvim wezterm navigator plugin

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,6 +10,45 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- NVIM MUX NAVIGATION
+
+local w = require("wezterm")
+local a = w.action
+
+local function is_inside_vim(pane)
+	local tty = pane:get_tty_name()
+	if tty == nil then
+		return false
+	end
+
+	local success, stdout, stderr = w.run_child_process({
+		"sh",
+		"-c",
+		"ps -o state= -o comm= -t"
+			.. w.shell_quote_arg(tty)
+			.. " | "
+			.. "grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?)(diff)?$'",
+	})
+
+	return success
+end
+
+local function is_outside_vim(pane)
+	return not is_inside_vim(pane)
+end
+
+local function bind_if(cond, key, mods, action)
+	local function callback(win, pane)
+		if cond(pane) then
+			win:perform_action(action, pane)
+		else
+			win:perform_action(a.SendKey({ key = key, mods = mods }), pane)
+		end
+	end
+
+	return { key = key, mods = mods, action = w.action_callback(callback) }
+end
+
 -- KEYBINDINGS
 config.keys = {
 	-- Remove unused defaults
@@ -244,6 +283,13 @@ config.keys = {
 		mods = "CTRL|SHIFT",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
+
+	bind_if(is_outside_vim, "h", "CTRL", a.ActivatePaneDirection("Left")),
+	bind_if(is_outside_vim, "l", "CTRL", a.ActivatePaneDirection("Right")),
+	bind_if(is_outside_vim, "k", "CTRL", a.ActivatePaneDirection("Up")),
+	bind_if(is_outside_vim, "j", "CTRL", a.ActivatePaneDirection("Down")),
+	bind_if(is_outside_vim, "[", "CTRL", a.ActivatePaneDirection("Prev")),
+	bind_if(is_outside_vim, "]", "CTRL", a.ActivatePaneDirection("Next")),
 }
 
 -- CONFIGURATION
