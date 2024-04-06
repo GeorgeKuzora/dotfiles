d9199bbcff98b570bb37721762cec3464c3b303e
GeorgiyKuzora <rafale87@gmail.com>
2024-04-06 08:40:00 +0300

    nvim: replace oil refresh keymap to "gl"
    In order to use <C-l> as window navigation key in oil buffer

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -4,8 +4,12 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {},
   config = function()
-    require('oil').setup {}
+    require('oil').setup { keymaps = {
+      ['<C-l>'] = false,
+      ['gl'] = 'actions.refresh',
+    } }
     local o = require 'oil'
+
     vim.keymap.set('n', '-', function()
       o.open()
     end, { desc = 'Oil open parent directory' })
