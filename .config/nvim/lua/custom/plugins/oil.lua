602035b7b5d330e012e941209cc73a2fd30a5883
GeorgiyKuzora <rafale87@gmail.com>
2024-06-02 11:05:42 +0300

    nvim: add keymap to oil float

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -4,14 +4,23 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {},
   config = function()
-    require('oil').setup { keymaps = {
-      ['<C-l>'] = false,
-      ['gl'] = 'actions.refresh',
-    } }
     local o = require 'oil'
+    o.setup {
+      keymaps = {
+        ['<C-l>'] = false,
+        ['gl'] = 'actions.refresh',
+      },
+      columns = { 'icon' },
+      view_options = {
+        show_hidden = true,
+      },
+    }
 
     vim.keymap.set('n', '-', function()
       o.open()
     end, { desc = 'Oil open parent directory' })
+    vim.keymap.set('n', '<leader>-', function()
+      o.toggle_float()
+    end, { desc = 'Oil open parent directory in float' })
   end,
 }
