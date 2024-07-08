177fc54f67a2d29880185a254f59d916679d434a
GeorgiyKuzora <rafale87@gmail.com>
2024-07-08 14:08:16 +0300

    nvim: Update better-escape config

--- .config/nvim/lua/custom/plugins/better-escape.lua
+++ .config/nvim/lua/custom/plugins/better-escape.lua
@@ -1,13 +1,30 @@
 return {
   'max397574/better-escape.nvim',
-  lazy = true,
-  event = 'InsertCharPre',
-  opts = {
-    timeout = 300,
-    mapping = { 'ii', 'шш' },
-    clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-    keys = function()
+  config = function()
+    local escape = function()
+      vim.api.nvim_input '<esc>'
+      local current_line = vim.api.nvim_get_current_line()
+      if current_line:match '^%s+i$' or current_line:match '^%s+ш$' then
+        vim.schedule(function()
+          vim.api.nvim_set_current_line ''
+        end)
+      end
       return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-    end, -- keys used for escaping, if it is a function will use the result everytime
-  },
+    end
+    require('better_escape').setup {
+      timeout = vim.o.timeoutlen,
+      mappings = {
+        i = {
+          i = {
+            -- These can all also be functions
+            i = escape,
+          },
+          ш = {
+            -- These can all also be functions
+            ш = escape,
+          },
+        },
+      },
+    }
+  end,
 }
