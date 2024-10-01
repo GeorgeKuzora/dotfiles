5565fa5cb8392aec027c9540b29da4460ba7b4cf
GeorgiyKuzora <rafale87@gmail.com>
2024-10-01 20:53:51 +0300

    nvim: ufo plagin update

--- .config/nvim/lua/custom/plugins/ufo.lua
+++ .config/nvim/lua/custom/plugins/ufo.lua
@@ -4,11 +4,6 @@ return {
     'kevinhwang91/promise-async',
   },
   config = function()
-    -- local ftMap = {
-    --     vim = 'indent',
-    --     python = {'indent'},
-    --     git = ''
-    -- }
     local handler = function(virtText, lnum, endLnum, width, truncate)
       local newVirtText = {}
       local suffix = (' 󰁂 %d '):format(endLnum - lnum)
@@ -38,22 +33,7 @@ return {
     end
 
     require('ufo').setup {
-      open_fold_hl_timeout = 150,
-      close_fold_kinds_for_ft = { 'imports', 'comment' },
       fold_virt_text_handler = handler,
-      preview = {
-        win_config = {
-          border = { '', '─', '', '', '', '─', '', '' },
-          winhighlight = 'Normal:Folded',
-          winblend = 0,
-        },
-        mappings = {
-          scrollU = '<C-u>',
-          scrollD = '<C-d>',
-          jumpTop = '[',
-          jumpBot = ']',
-        },
-      },
       provider_selector = function(bufnr, filetype, buftype)
         return { 'treesitter', 'indent' }
       end,
