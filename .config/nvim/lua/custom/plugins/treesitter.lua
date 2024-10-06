1c3aa1ddb3f54ccf939da370bfab3906a5d6e205
GeorgiyKuzora <rafale87@gmail.com>
2024-10-06 21:10:13 +0300

    nvim: add description to go to context

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -193,6 +193,6 @@ return {
     }
     vim.keymap.set('n', 'gs', function()
       require('treesitter-context').go_to_context(vim.v.count1)
-    end, { silent = true })
+    end, { desc= 'Go to context', silent = true })
   end, 0),
 }
