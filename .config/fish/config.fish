33019baf179bab1d9e19ed7448c3fb7c8262a086
GeorgiyKuzora <rafale87@gmail.com>
2024-10-13 11:05:10 +0300

    fish: set hybrid binds and safe abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,15 @@
-set fish_greeting
-set -g fish_key_bindings fish_vi_key_bindings
-# Emulates vim's cursor shape behavior
-# Set the normal and visual mode cursors to a block
+# OPTIONS
+set fish_greeting # disable greeting on startup
+
+# KEYBINDINGS
+set -g fish_key_bindings fish_hybrid_key_bindings
+
+# CURSOR BEHAVIOR
 set fish_cursor_default block
-# Set the insert mode cursor to a line
 set fish_cursor_insert line
-# Set the replace mode cursors to an underscore
 set fish_cursor_replace_one underscore
 set fish_cursor_replace underscore
-# Set the external cursor to a line. The external cursor appears when a command is started.
-# The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
 set fish_cursor_external line
-# The following variable can be used to configure cursor shape in
-# visual mode, but due to fish_cursor_default, is redundant here
 set fish_cursor_visual block
 set -g fish_vi_force_cursor 1
 
@@ -41,47 +38,45 @@ fish_add_path $PYENV_ROOT/bin $PATH $GOPATH/bin $HOME/.local/bin
 
 # ABBREVIATION
 # Colorize grep output (good for log files)
-abbr -a grep 'grep --color=auto'
-abbr -a egrep 'egrep --color=auto'
-abbr -a fgrep 'fgrep --color=auto'
+safe_abbr grep 'grep --color=auto'
+safe_abbr egrep 'egrep --color=auto'
+safe_abbr fgrep 'fgrep --color=auto'
 # easier to read disk
-abbr -a df 'df -h'     # human-readable sizes
-abbr -a free 'free -m' # show sizes in MB
+safe_abbr df 'df -h'     # human-readable sizes
+safe_abbr free 'free -m' # show sizes in MB
 # nvim to vim
-abbr -a vi 'nvim'
-abbr -a vim 'nvim'
-abbr -a vin 'neovide'
+safe_abbr vi nvim
+safe_abbr vim nvim
+safe_abbr vin neovide
 # system
-abbr -a update 'sudo dnf upgrade && sudo snap refresh && flatpack update'
+safe_abbr update 'sudo dnf upgrade && sudo snap refresh && flatpack update'
 #tools
-abbr -a zj 'zellij'
-abbr -a zja 'zellij a'
-abbr -a zjs 'zellij -s'
-abbr -a lg 'lazygit'
-abbr -a lzg 'lazygit'
-abbr -a lzd 'lazydocker'
+safe_abbr zj 'zellij'
+safe_abbr zja 'zellij a'
+safe_abbr zjs 'zellij -s'
+safe_abbr lg 'lazygit'
+safe_abbr lzg 'lazygit'
+safe_abbr lzd 'lazydocker'
 # Git
-abbr -a gst git status
-abbr -a gl git log
-abbr -a ga git add
-abbr -a gc git commit
-abbr -a gP git push
-abbr -a gp git pull
-abbr -a gf git fetch
-abbr -a gco git checkout
-abbr -a gcb git checkout -b
-abbr -a gwta git worktree add
-abbr -a gwtr git worktree remove
+safe_abbr gst 'git status'
+safe_abbr gl 'git log'
+safe_abbr ga 'git add'
+safe_abbr gc 'git commit'
+safe_abbr gP 'git push'
+safe_abbr gp 'git pull'
+safe_abbr gf 'git fetch'
+safe_abbr gco 'git checkout'
+safe_abbr gcb 'git checkout -b'
+safe_abbr gwta 'git worktree add'
+safe_abbr gwtr 'git worktree remove'
 # Modern versions of classic tools
-if type -q exa; and type -q bat
-    abbr -a ls 'exa --oneline'
-    abbr -a ll 'exa -lah --color=auto --group-directories-first --icons --git --header'   # show long listing of all except ".."
-    abbr -a cat 'bat'
-else
-    abbr -a ls 'ls --color=auto'
-    abbr -a ll 'ls -lavh --ignore=.. --color=auto --group-directories-first'   # show long listing of all except ".."
-    abbr -a l 'ls -lavh --ignore=.?* --color=auto --group-directories-first'   # show long listing but no hidden dotfiles except "."
-end
+safe_abbr ls 'ls --color=auto'
+safe_abbr ll 'ls -lavh --ignore=.. --color=auto --group-directories-first'   # show long listing of all except ".."
+safe_abbr l 'ls -lavh --ignore=.?* --color=auto --group-directories-first'   # show long listing but no hidden dotfiles except "."
+safe_abbr ls 'exa --oneline'
+safe_abbr ll 'exa -lah --color=auto --group-directories-first --icons --git --header'   # show long listing of all except ".."
+safe_abbr cat 'bat'
+
 
 if status is-interactive
     atuin init fish | source
