de33e9ca2197c01592032939f227c99dc95ba106
GeorgiyKuzora <g.kuzora@korona.net>
2024-10-16 16:56:06 +0300

    nix: set wsl profile flake

---
+++ .nix/wsl/profile.nix
@@ -0,0 +1,40 @@
+{
+  description = "WSL profile flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x87_64-linux; [
+        atuin
+        bat
+        delta
+        fd
+        fzf
+        go
+        lazygit
+        lua
+        luajitPackages.luarocks
+        neovim
+        nodejs
+        ripgrep
+        starship
+        stow
+        unzip
+        xclip
+        zoxide
+        eza
+        lazydocker
+        yazi
+        jq
+        direnv
+        fish
+        werf
+        task-go
+        httpie
+    ];
+  };
+}
+
