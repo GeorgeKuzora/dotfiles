85960bcf1d0df47e1c5e16ec3c6a0affb1d9c542
GeorgiyKuzora <g.kuzora@korona.net>
2024-10-22 14:03:56 +0300

    fish: add pyal abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -75,6 +75,7 @@ safe_abbr l 'ls -lavh --ignore=.?* --color=auto --group-directories-first'   # s
 safe_abbr ls 'exa --oneline'
 safe_abbr ll 'exa -lah --color=auto --group-directories-first --icons --git --header'   # show long listing of all except ".."
 safe_abbr cat 'bat'
+safe_abbr pyal 'source .venv/bin/activate.fish'
 
 
 if status is-interactive
