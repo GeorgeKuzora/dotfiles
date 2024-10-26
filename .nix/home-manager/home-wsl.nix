c74d9f9a63b59d87edb2ab42549fc9cfab561f3d
kuzora <kuzora@BOOK09L980.win.ftc.ru>
2024-10-26 18:15:33 +0300

    nix: wsl remove unused links

--- .nix/home-manager/home-wsl.nix
+++ .nix/home-manager/home-wsl.nix
@@ -47,28 +47,12 @@ in {
 
     file = {
       ".config/atuin".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/atuin";
-      ".config/autostart".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/autostart";
-
-      ".config/Code/User/snippets".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/Code/User/snippets";
-      ".config/Code/User/init.lua".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/Code/User/init.lua";
-      ".config/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/Code/User/settings.json";
-      ".config/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/Code/User/keybindings.json";
-
       ".config/fish".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/fish";
       ".config/git".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/git";
-      ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/kitty";
-      ".config/mpv".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/mpv";
       ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/nvim";
       ".config/pypoerty".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/pypoetry";
       ".config/starship".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/starship";
       ".config/tmux/tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/tmux/tmux.conf";
-      ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/wezterm";
-      ".config/zed".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/zed";
-      ".config/zellij".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/zellij";
-
-      ".config/zsh/.zshrc".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/zsh/.zshrc";
-      ".config/zsh/vendor-completions".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.config/zsh/vendor-completions";
-      ".zshenv".source = config.lib.file.mkOutOfStoreSymlink "${home}/.dotfiles/.zshenv";
     };
     sessionVariables = {
       # EDITOR = "emacs";
