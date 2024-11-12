f0480917a4db2fc68181a01b5d3a4ac693aea8ac
GeorgiyKuzora <g.kuzora@korona.net>
2024-11-12 14:57:46 +0300

    nvim: hardtime enable arrows keys for graphite layout

--- .config/nvim/lua/custom/plugins/hardtime.lua
+++ .config/nvim/lua/custom/plugins/hardtime.lua
@@ -1,7 +1,13 @@
 return {
-   "m4xshen/hardtime.nvim",
-   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-   opts = {
-      disable_mouse=false,
-   }
+  'm4xshen/hardtime.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+  opts = {
+    disable_mouse = false,
+    disabled_keys = {
+      ['<Up>'] = {},
+      ['<Down>'] = {},
+      ['<Left>'] = {},
+      ['<Right>'] = {},
+    },
+  },
 }
