92e8adc09f7e8d7211d2f160888f00b9a5eb0691
GeorgiyKuzora <g.kuzora@korona.net>
2024-11-12 15:13:52 +0300

    nvim: add diffview keymaps

--- .config/nvim/lua/custom/plugins/neogit.lua
+++ .config/nvim/lua/custom/plugins/neogit.lua
@@ -10,10 +10,12 @@ return {
     config = function()
       local neogit = require 'neogit'
       neogit.setup()
-      vim.keymap.set('n', '<leader>gg', neogit.open, { desc = 'Open [N]eo[G]it' })
+      vim.keymap.set('n', '<leader>gg', neogit.open, { desc = 'Open Neogit' })
       vim.keymap.set('n', '<leader>gG', function()
         neogit.open { 'commit' }
       end, { desc = 'Open [N]eo[G]it commit dialog' })
+      vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen<cr>', { desc = 'Open Diffview' })
+      vim.keymap.set('n', '<leader>gk', '<cmd>DiffviewClose<cr>', { desc = 'Close Diffview' })
     end,
   },
 }
