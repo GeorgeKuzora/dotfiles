5f7a201200efa12165542e51675d94f2dfcc4673
GeorgiyKuzora <g.kuzora@korona.net>
2024-11-30 10:57:00 +0300

    nvim: set debuger keymaps

--- .config/nvim/lua/custom/plugins/lsp/debug.lua
+++ .config/nvim/lua/custom/plugins/lsp/debug.lua
@@ -42,22 +42,24 @@ return {
         },
       },
     }
-
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = 'red', linehl = '', numhl = '' })
 
     require('dap-go').setup()
     require('dap-python').setup '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
 
+    vim.keymap.set('n', '<F2>', dap.restart, { desc = 'Debug: Restart' })
     vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<F4>', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<F6>', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Breakpoint with condition' })
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    vim.keymap.set('n', '<F8>', dap.pause, { desc = 'Debug: Pause' })
+    vim.keymap.set('n', '<F9>', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<F11>', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'Debug: Step Out' })
   end,
 }
