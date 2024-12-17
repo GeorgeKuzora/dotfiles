2611c03253d54b6af5a0a0280ef0b5d9183a945b
GeorgiyKuzora <g.kuzora@korona.net>
2024-12-17 17:46:38 +0300

    nvim: luasnip upgrade

--- .config/nvim/lua/custom/plugins/luasnip.lua
+++ .config/nvim/lua/custom/plugins/luasnip.lua
@@ -1,57 +1,61 @@
 return {
-    'L3MON4D3/LuaSnip',
-    name = 'luasnip',
-    -- version = "v2.*",
-    build = "make install_jsregexp",
-    dependencies = {
-        'rafamadriz/friendly-snippets',
-    },
-    config = function()
-    local ls = require "luasnip"
-    local types = require "luasnip.util.types"
+  'L3MON4D3/LuaSnip',
+  name = 'luasnip',
+  version = 'v2.*',
+  build = 'make install_jsregexp',
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+  },
+  config = function()
+    local ls = require 'luasnip'
+    local types = require 'luasnip.util.types'
 
     ls.config.set_config {
-        -- This tells LuaSnip to remember to keep around the last snippet.
-        -- You can jump back into it even if you move outside of the selection
-        history = false,
+      -- This tells LuaSnip to remember to keep around the last snippet.
+      -- You can jump back into it even if you move outside of the selection
+      history = false,
 
-        -- This one is cool cause if you have dynamic snippets, it updates as you type!
-        updateevents = "TextChanged,TextChangedI",
+      -- This one is cool cause if you have dynamic snippets, it updates as you type!
+      updateevents = 'TextChanged,TextChangedI',
 
-        -- Autosnippets:
-        enable_autosnippets = true,
+      -- Autosnippets:
+      enable_autosnippets = true,
 
-        -- Crazy highlights!!
-        -- #vid3
-        -- ext_opts = nil,
-        ext_opts = {
-            [types.choiceNode] = {
-                active = {
-                    virt_text = { { " <- ", "NonTest" } },
-                },
-            },
+      -- Crazy highlights!!
+      -- #vid3
+      -- ext_opts = nil,
+      ext_opts = {
+        [types.choiceNode] = {
+          active = {
+            virt_text = { { ' <- ', 'NonTest' } },
+          },
         },
+      },
     }
 
-    vim.keymap.set({ "i", "s" }, "<c-j>", function()
-        if ls.expand_or_jumpable() then
-            ls.expand_or_jump()
-        end
+    require('luasnip.loaders.from_vscode').lazy_load()
+    require('luasnip.loaders.from_vscode').lazy_load { paths = '~/.config/nvim/lua/custom/snippets' }
+    require('luasnip').filetype_extend('python', { 'pydoc', 'django', 'django-rest' })
+    require('luasnip').filetype_extend('html', { 'djangohtml', 'htmldjango' })
+
+    vim.keymap.set({ 'i', 's' }, '<c-j>', function()
+      if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+      end
     end, { silent = true })
 
-    vim.keymap.set({ "i", "s" }, "<c-k>", function()
-        if ls.jumpable(-1) then
-            ls.jump(-1)
-        end
+    vim.keymap.set({ 'i', 's' }, '<c-k>', function()
+      if ls.jumpable(-1) then
+        ls.jump(-1)
+      end
     end, { silent = true })
 
-    vim.keymap.set({ "i", "s" }, "<c-l>", function()
-        if ls.choice_active() then
-            ls.change_choice(1)
-        end
+    vim.keymap.set({ 'i', 's' }, '<c-l>', function()
+      if ls.choice_active() then
+        ls.change_choice(1)
+      end
     end, { silent = true })
 
-    vim.keymap.set("n", "<leader>m", "<cmd>source ~/.config/nvim/lua/custom/plugins/luasnip.lua<CR>",
-        { desc = "Source Luasnip file" })
-    end
+    vim.keymap.set('n', '<leader>m', '<cmd>source ~/.config/nvim/lua/custom/plugins/luasnip.lua<CR>', { desc = 'Source Luasnip file' })
+  end,
 }
