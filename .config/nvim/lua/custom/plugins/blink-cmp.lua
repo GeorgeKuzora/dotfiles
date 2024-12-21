2db45971b1021db08f040e08c990cf643c80cd7e
GeorgiyKuzora <g.kuzora@korona.net>
2024-12-21 18:56:15 +0300

    nvim: add trigger show in smippet for blink

--- .config/nvim/lua/custom/plugins/blink-cmp.lua
+++ .config/nvim/lua/custom/plugins/blink-cmp.lua
@@ -11,6 +11,11 @@ return {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono'
     },
+    completion = {
+      trigger = {
+        show_in_snippet = false,
+      },
+    },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
