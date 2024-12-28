9e860109fd1bdb257bbe5a3200cf71684d4fda43
GeorgiyKuzora <g.kuzora@korona.net>
2024-12-28 08:53:44 +0300

    fish: set vim to main nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,10 +47,10 @@ safe_abbr df 'df -h' # human-readable sizes
 safe_abbr free 'free -m' # show sizes in MB
 # Nvim
 safe_abbr vi nvim
-safe_abbr vim nvim
-safe_abbr vin neovide
-safe_abbr lazyvim 'set -gx NVIM_APPNAME lazyvim; nvim'
+safe_abbr vim 'set -gx NVIM_APPNAME nvim; nvim'
 safe_abbr vid 'set -gx NVIM_APPNAME defaultvim; nvim'
+safe_abbr lazyvim 'set -gx NVIM_APPNAME lazyvim; nvim'
+safe_abbr vin neovide
 # System
 safe_abbr update 'sudo dnf update && sudo snap refresh && flatpak update'
 # Git
