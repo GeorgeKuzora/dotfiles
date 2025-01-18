380538fe52ec6014bc595f11e939c6bbe708246c
GeorgiyKuzora <rafale87@gmail.com>
2025-01-18 21:47:49 +0300

    nix: chnges

--- .nix/home-manager/flake.nix
+++ .nix/home-manager/flake.nix
@@ -3,14 +3,16 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
