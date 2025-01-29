2e08ad85d3e7c729dea57779ce914f31ac81f476
GeorgiyKuzora <rafale87@gmail.com>
2025-01-29 22:51:48 +0300

    nvim: disable rainbow delimeters

--- .config/nvim/lua/custom/plugins/rainbow-delimiters.lua
+++ .config/nvim/lua/custom/plugins/rainbow-delimiters.lua
@@ -1,28 +1,28 @@
 return {
-  'hiphish/rainbow-delimiters.nvim',
-  name = 'rainbow_delimiters',
-  event = { 'BufReadPre', 'BufNewFile' },
-  config = function()
-    local rainbow_delimiters = require 'rainbow-delimiters'
-
-    vim.g.rainbow_delimiters = {
-      strategy = {
-        [''] = rainbow_delimiters.strategy['global'],
-        vim = rainbow_delimiters.strategy['local'],
-      },
-      query = {
-        [''] = 'rainbow-delimiters',
-        lua = 'rainbow-blocks',
-      },
-      highlight = {
-        'RainbowDelimiterRed',
-        'RainbowDelimiterOrange',
-        'RainbowDelimiterYellow',
-        'RainbowDelimiterGreen',
-        'RainbowDelimiterBlue',
-        'RainbowDelimiterViolet',
-        'RainbowDelimiterCyan',
-      },
-    }
-  end,
+  -- 'hiphish/rainbow-delimiters.nvim',
+  -- name = 'rainbow_delimiters',
+  -- event = { 'BufReadPre', 'BufNewFile' },
+  -- config = function()
+  --   local rainbow_delimiters = require 'rainbow-delimiters'
+  --
+  --   vim.g.rainbow_delimiters = {
+  --     strategy = {
+  --       [''] = rainbow_delimiters.strategy['global'],
+  --       vim = rainbow_delimiters.strategy['local'],
+  --     },
+  --     query = {
+  --       [''] = 'rainbow-delimiters',
+  --       lua = 'rainbow-blocks',
+  --     },
+  --     highlight = {
+  --       'RainbowDelimiterRed',
+  --       'RainbowDelimiterOrange',
+  --       'RainbowDelimiterYellow',
+  --       'RainbowDelimiterGreen',
+  --       'RainbowDelimiterBlue',
+  --       'RainbowDelimiterViolet',
+  --       'RainbowDelimiterCyan',
+  --     },
+  --   }
+  -- end,
 }
