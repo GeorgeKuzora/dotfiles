2a43bc0ad9477cd4b5474464dad3f7d57a3113b5
GeorgiyKuzora <g.kuzora@korona.net>
2025-02-15 10:45:37 +0300

    nvim: move plugins in init.lua

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -4,4 +4,20 @@ return {
   'rafamadriz/friendly-snippets',
   'nvim-tree/nvim-web-devicons',
   'sindrets/diffview.nvim',
+  {
+    'stevearc/dressing.nvim',
+    opts = {
+      input = { default_prompt = '➤ ' },
+      select = { backend = { 'telescope', 'builtin' } },
+    },
+  },
+  {
+    'j-hui/fidget.nvim',
+    opts = {},
+  },
+  {
+    'catgoose/nvim-colorizer.lua',
+    event = 'BufReadPre',
+    opts = {},
+  },
 }
--- .config/nvim/lua/custom/plugins/nvim-colorizer.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'NvChad/nvim-colorizer.lua',
-  event = { 'BufReadPre', 'BufNewFile' },
-  cmd = { 'ColorizerToggle', 'ColorizerAttachToBuffer', 'ColorizerDetachFromBuffer', 'ColorizerReloadAllBuffers' },
-  opts = { user_default_options = { names = false } },
-  config = true,
-}
