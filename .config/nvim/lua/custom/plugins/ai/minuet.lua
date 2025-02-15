0d3ea361a4396a8aa273f0d396f14da4878f90f4
GeorgiyKuzora <g.kuzora@korona.net>
2025-02-15 17:59:50 +0300

    nvim: set minuet for work

--- .config/nvim/lua/custom/plugins/ai/minuet.lua
+++ .config/nvim/lua/custom/plugins/ai/minuet.lua
@@ -1,39 +1,43 @@
 return {
-  -- 'milanglacier/minuet-ai.nvim',
-  -- config = function()
-  --   require('minuet').setup {
-  --     provider = 'openai_fim_compatible',
-  --     n_completions = 1, -- recommend for local model for resource saving
-  --     context_window = 2048, -- adjust context window size for available computing resources
-  --     provider_options = {
-  --       openai_fim_compatible = {
-  --         api_key = 'TERM',
-  --         name = 'Ollama',
-  --         end_point = 'http://127.0.0.1:29841/v1/completions',
-  --         model = 'qwen2.5-coder:7b',
-  --         optional = {
-  --           max_tokens = 256,
-  --           top_p = 0.9,
-  --         },
-  --       },
-  --     },
-  --     virtualtext = {
-  --       auto_trigger_ft = {},
-  --       keymap = {
-  --         -- accept whole completion
-  --         accept = '<s-Tab>',
-  --         -- accept one line
-  --         accept_line = '<Tab>',
-  --         -- accept n lines (prompts for number)
-  --         -- e.g. "A-z 2 CR" will accept 2 lines
-  --         accept_n_lines = '<A-z>',
-  --         -- Cycle to prev completion item, or manually invoke completion
-  --         prev = '<A-[>',
-  --         -- Cycle to next completion item, or manually invoke completion
-  --         next = '<A-]>',
-  --         dismiss = '<A-e>',
-  --       },
-  --     },
-  --   }
-  -- end,
+  'milanglacier/minuet-ai.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    require('minuet').setup {
+      provider = 'openai_fim_compatible',
+      n_completions = 1, -- recommend for local model for resource saving
+      context_window = 1024, -- adjust context window size for available computing resources
+      provider_options = {
+        openai_fim_compatible = {
+          api_key = 'TERM',
+          name = 'Ollama',
+          end_point = 'http://127.0.0.1:29841/v1/completions',
+          model = 'qwen2.5-coder:7b',
+          optional = {
+            max_tokens = 256,
+            top_p = 0.9,
+          },
+        },
+      },
+      virtualtext = {
+        auto_trigger_ft = { 'python', 'lua', 'go' },
+        show_on_completion_menu = true,
+        keymap = {
+          -- accept whole completion
+          accept = '<A-A>',
+          -- accept one line
+          accept_line = '<A-a>',
+          -- accept n lines (prompts for number)
+          -- e.g. "A-z 2 CR" will accept 2 lines
+          accept_n_lines = '<A-z>',
+          -- Cycle to prev completion item, or manually invoke completion
+          prev = '<A-[>',
+          -- Cycle to next completion item, or manually invoke completion
+          next = '<A-]>',
+          dismiss = '<A-e>',
+        },
+      },
+    }
+  end,
 }
