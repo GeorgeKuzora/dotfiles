5df16dacaf19a86cc86ddd4a0dc2a39e579cd6de
GeorgiyKuzora <g.kuzora@korona.net>
2025-02-16 15:10:54 +0300

    nvim: disable supermaven

--- .config/nvim/lua/custom/plugins/ai/supermaven.lua
+++ .config/nvim/lua/custom/plugins/ai/supermaven.lua
@@ -1,13 +1,13 @@
 return {
-  'supermaven-inc/supermaven-nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  config = function()
-    require('supermaven-nvim').setup {
-      keymaps = {
-        accept_suggestion = '<Tab>',
-        clear_suggestion = '<C-]>',
-        accept_word = '<C-j>',
-      },
-    }
-  end,
+  -- 'supermaven-inc/supermaven-nvim',
+  -- event = { 'BufReadPre', 'BufNewFile' },
+  -- config = function()
+  --   require('supermaven-nvim').setup {
+  --     keymaps = {
+  --       accept_suggestion = '<Tab>',
+  --       clear_suggestion = '<C-]>',
+  --       accept_word = '<C-j>',
+  --     },
+  --   }
+  -- end,
 }
