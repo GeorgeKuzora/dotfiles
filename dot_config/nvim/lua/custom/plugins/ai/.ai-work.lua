bb855b51e7db8481a754468fc739f3de8577a6cc
GeorgiyKuzora <rafale87@gmail.com>
2025-04-09 06:56:18 +0300

    nvim: add avante to work config

--- dot_config/nvim/lua/custom/plugins/ai/.ai-work.lua
+++ dot_config/nvim/lua/custom/plugins/ai/.ai-work.lua
@@ -46,6 +46,44 @@ return {
 --     vim.cmd('Minuet virtualtext disable')
 --   end,
 -- },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      provider = "koronatech",
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0,
+        max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      },
+      vendors = {
+        koronatech = {
+          __inherited_from = "openai",
+          api_key_name = "KORONATECH_API_KEY",
+          endpoint = "https://ai.ftc.ru/api/chat/v1",
+          model = "instruct",
+        },
+      },
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
   -- {
   -- 'olimorris/codecompanion.nvim',
   -- event = { 'BufReadPre', 'BufNewFile' },
