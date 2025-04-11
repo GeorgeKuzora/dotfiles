7c7f1e6f18527bc0b62cfbef277a9a1a29f220dd
GeorgiyKuzora <g.kuzora@korona.net>
2025-04-11 11:52:52 +0300

    nvim: fix ai url

--- dot_config/nvim/lua/custom/plugins/ai/.ai-work.lua
+++ dot_config/nvim/lua/custom/plugins/ai/.ai-work.lua
@@ -1,4 +1,48 @@
 return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    opts = {
+      provider = "koronatech",
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o",  -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0,
+        max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      },
+      vendors = {
+        koronatech = {
+          __inherited_from = "openai",
+          api_key_name = "KORONATECH_API_KEY",
+          endpoint = "https://ai.kth.pro/api/v1",
+          model = "Instruct",
+        },
+      },
+      file_selector = {
+        provider = "snacks",
+        -- Options override for custom providers
+        provider_opts = {},
+      }
+    },
+  },
   -- {
   --   'milanglacier/minuet-ai.nvim',
   --   dependencies = {
@@ -46,50 +90,6 @@ return {
   --     vim.cmd('Minuet virtualtext disable')
   --   end,
   -- },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-    opts = {
-      provider = "koronatech",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o",  -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-      },
-      vendors = {
-        koronatech = {
-          __inherited_from = "openai",
-          api_key_name = "KORONATECH_API_KEY",
-          endpoint = "https://ai.kth.ru/api",
-          model = "Instruct",
-        },
-      },
-      file_selector = {
-        provider = "snacks",
-        -- Options override for custom providers
-        provider_opts = {},
-      }
-    },
-  },
   -- {
   -- 'olimorris/codecompanion.nvim',
   -- event = { 'BufReadPre', 'BufNewFile' },
