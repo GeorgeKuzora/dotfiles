4e3609089f27c3cd2485e29c903d364f2434ebe0
GeorgiyKuzora <rafale87@gmail.com>
2025-04-11 20:10:08 +0300

    nvim: update ai work

--- dot_config/nvim/lua/custom/plugins/ai/.ai-work.lua
+++ dot_config/nvim/lua/custom/plugins/ai/.ai-work.lua
@@ -21,10 +21,10 @@ return {
     opts = {
       provider = "koronatech",
       openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o",  -- your desired model (or use gpt-4o, etc.)
+        endpoint = "https://ai.kth.pro/api/v1",
+        model = "Instruct",  -- your desired model (or use gpt-4o, etc.)
         timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
+        temperature = 0.8,
         max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
         --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
       },
