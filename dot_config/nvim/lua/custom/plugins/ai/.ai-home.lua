840dcff5f92a3d9fe069f337119f6002632a2739
GeorgiyKuzora <rafale87@gmail.com>
2025-04-11 20:14:59 +0300

    nvim: set avante home

--- dot_config/nvim/lua/custom/plugins/ai/.ai-home.lua
+++ dot_config/nvim/lua/custom/plugins/ai/.ai-home.lua
@@ -15,50 +15,51 @@ return {
       }
     end,
   },
-  -- {
-  --   "yetone/avante.nvim",
-  --   event = "VeryLazy",
-  --   version = false, -- Never set this value to "*"! Never!
-  --   opts = {
-  --     -- add any opts here
-  --     -- for example
-  --     provider = "mistral",
-  --     openai = {
-  --       endpoint = "https://api.openai.com/v1",
-  --       model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-  --       timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-  --       temperature = 0,
-  --       max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-  --       --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-  --     },
-  --     vendors = {
-  --       mistral= {
-  --         __inherited_from = "openai",
-  --         api_key_name = "MISTRAL_API_KEY",
-  --         endpoint = "https://api.mistral.ai/v1/",
-  --         model = "mistral-large-latest",
-  --         max_tokens = 4096, -- to avoid using max_completion_tokens
-  --       },
-  --     },
-  --   },
-  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  --   build = "make",
-  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "stevearc/dressing.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     {
-  --       -- Make sure to set this up properly if you have lazy=true
-  --       'MeanderingProgrammer/render-markdown.nvim',
-  --       opts = {
-  --         file_types = { "markdown", "Avante" },
-  --       },
-  --       ft = { "markdown", "Avante" },
-  --     },
-  --   },
-  -- },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    opts = {
+      provider = "mistral",
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0.8,
+        max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      },
+      vendors = {
+        mistral= {
+          __inherited_from = "openai",
+          api_key_name = "MISTRAL_API_KEY",
+          endpoint = "https://api.mistral.ai/v1/",
+          model = "mistral-large-latest",
+          max_tokens = 4096, -- to avoid using max_completion_tokens
+        },
+      },
+      file_selector = {
+        provider = "snacks",
+        -- Options override for custom providers
+        provider_opts = {},
+      }
+    },
+  },
   -- {
   -- 'olimorris/codecompanion.nvim',
   -- event = { 'BufReadPre', 'BufNewFile' },
