31f4116d5f4252cd4f6cd739a705c8f57dbf5b9e
GeorgiyKuzora <rafale87@gmail.com>
2025-04-11 20:01:37 +0300

    nvim: move configs

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,8 +2,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 for _, source in ipairs {
-  'custom.core',
-  'custom.lazy',
+  'custom.config',
 } do
   local status_ok, fault = pcall(require, source)
   if not status_ok then
--- dot_config/nvim/lua/custom/core/init.lua
+++
@@ -1,4 +0,0 @@
-require 'custom.core.keymaps'
-require 'custom.core.options'
-require 'custom.core.config'
-require 'custom.core.autocmds'
