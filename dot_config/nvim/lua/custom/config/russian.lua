cf447234132c7fcd6ee8871d345e5f19b48ea9c2
GeorgiyKuzora <g.kuzora@korona.net>
2025-06-20 10:18:07 +0300

    nvim: add russian redo

--- dot_config/nvim/lua/custom/config/russian.lua
+++ dot_config/nvim/lua/custom/config/russian.lua
@@ -11,4 +11,5 @@ km.set('i', 'шш', '<esc>', { desc = 'Escape insert mode' })
 km.set('n', '<leader>щ', 'o<Esc>k', { desc = 'Insert empty line bellow' })
 km.set('n', 'л', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 km.set('n', 'о', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+km.set('n', '<C-к>', '<C-r>', { desc = 'Redo' })
 
