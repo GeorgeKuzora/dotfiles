9b0289d79f1063a9bacdef3ac2b645b8ae98e69c
GeorgiyKuzora <g.kuzora@korona.net>
2025-10-29 14:00:07 +0300

    nvim: add lsp server configs

--- dot_config/nvim/after/lsp/basedpyright.lua
+++ dot_config/nvim/after/lsp/basedpyright.lua
@@ -1,9 +1,28 @@
 return {
+  cmd = { 'basedpyright-langserver', '--stdio' },
+  filetypes = { 'python' },
+  root_markers = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    'pyrightconfig.json',
+    '.git',
+    '.jj',
+  },
   settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = 'openFilesOnly',
+      },
+    },
     python = {
       analysis = {
         diagnosticMode = "workspace",
-        typeCheckingMode = "basic",
+        typeCheckingMode = "recommended",
         exclude = { "hidden*", ".venv" },
         allowedUntypedLibraries = { "mdw_tracing", "app.system.tracing" },
       },
