f3c0b26a7b2b55aa1eff5f85fde4dcc775527757
GeorgiyKuzora <g.kuzora@korona.net>
2025-11-05 21:56:19 +0300

    nvim: clean

--- dot_config/nvim/lua/custom/autocmds/indentation_by_filetype.lua
+++ dot_config/nvim/lua/custom/autocmds/indentation_by_filetype.lua
@@ -3,7 +3,7 @@ local auto_group = AutocmdGroup 'Indent'
 -- Set filetype-specific settings
 vim.api.nvim_create_autocmd("FileType", {
   group = auto_group,
-  pattern = { "python", "golang", "go", "rust", "rst" },
+  pattern = { "python", "py", "golang", "go", "rust", "rst", "rs" },
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -1,16 +1,8 @@
 return {
   'neovim/nvim-lspconfig',
   'onsails/lspkind.nvim',
-  'tpope/vim-sleuth',
   'rafamadriz/friendly-snippets',
   'nvim-tree/nvim-web-devicons',
-  {
-    'stevearc/dressing.nvim',
-    opts = {
-      input = { default_prompt = '➤ ' },
-      select = { backend = { 'telescope', 'builtin' } },
-    },
-  },
   {
     'j-hui/fidget.nvim',
     opts = {},
