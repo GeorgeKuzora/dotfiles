22447238ee97fa6e4c49a44e479505ed9904e81d
GeorgiyKuzora <g.kuzora@korona.net>
2025-11-19 16:41:10 +0300

    nvim: set catppucine underlines

--- dot_config/nvim/lua/custom/plugins/catppuccin.lua
+++ dot_config/nvim/lua/custom/plugins/catppuccin.lua
@@ -32,6 +32,18 @@ return {
         types = { 'italic' },
         operators = {},
       },
+      lsp_styles = {
+        underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+            ok = { "undercurl" },
+        },
+        inlay_hints = {
+            background = false,
+        },
+      },
       color_overrides = {
         mocha = {
           base = '#181825',
@@ -40,41 +52,23 @@ return {
         },
       },
       integrations = {
-        alpha = true,
         blink_cmp = true,
         diffview = true,
         dropbar = {
           enabled = true,
-          color_mode = true, -- enable color for kind's texts, not just kind's icons
+          color_mode = true,
         },
-        fidget = true,
         gitsigns = true,
-        grug_far = true,
         harpoon = true,
         indent_blankline = {
           enabled = true,
         },
         lsp_trouble = true,
         mason = true,
-        mini = {
-          enabled = true,
-        },
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { 'undercurl' },
-            hints = { 'undercurl' },
-            warnings = { 'undercurl' },
-            information = { 'undercurl' },
-          },
-        },
-        neogit = true,
         neotest = true,
         nvim_surround = true,
-        notify = true,
         semantic_tokens = true,
         snacks = true,
-        telescope = true,
         treesitter = true,
         treesitter_context = true,
         which_key = true,
