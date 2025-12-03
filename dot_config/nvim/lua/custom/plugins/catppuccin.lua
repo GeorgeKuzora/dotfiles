cf1a5de4b131d06a5f76f91a7b88d9dc9070efb9
GeorgiyKuzora <g.kuzora@korona.net>
2025-12-03 21:08:20 +0300

    nvim: catppuccin disable background override

--- dot_config/nvim/lua/custom/plugins/catppuccin.lua
+++ dot_config/nvim/lua/custom/plugins/catppuccin.lua
@@ -44,13 +44,13 @@ return {
             background = false,
         },
       },
-      color_overrides = {
-        mocha = {
-          base = '#181825',
-          mantle = '#11111b',
-          crust = '#11111b',
-        },
-      },
+      -- color_overrides = {
+      --   mocha = {
+      --     base = '#181825',
+      --     mantle = '#11111b',
+      --     crust = '#11111b',
+      --   },
+      -- },
       integrations = {
         blink_cmp = true,
         dap = true,
