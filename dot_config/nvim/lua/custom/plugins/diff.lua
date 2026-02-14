1a10c211bf65726babdc76351e59c06c5a228c9f
GeorgiyKuzora <g.kuzora@korona.net>
2026-02-14 09:18:33 +0300

    nvim: add hunk nvim

--- dot_config/nvim/lua/custom/plugins/diff.lua
+++ dot_config/nvim/lua/custom/plugins/diff.lua
@@ -1,11 +1,21 @@
 return {
-  'sindrets/diffview.nvim',
-  event = 'VeryLazy',
-  opts = {
-    view = {
-      merge_tool = {
-        layout = 'diff3_mixed',
+  {
+    'sindrets/diffview.nvim',
+    event = 'VeryLazy',
+    opts = {
+      view = {
+        merge_tool = {
+          layout = 'diff3_mixed',
+        },
       },
     },
+  },
+  {
+    "julienvincent/hunk.nvim",
+    cmd = { "DiffEditor" },
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = function()
+      require("hunk").setup()
+    end,
   },
 }
