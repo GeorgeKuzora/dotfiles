235b839674d1ed0c8435267311f334dbff8e771f
GeorgiyKuzora <georgiy@kuzora.ru>
2026-05-31 22:05:29 +0300

    nvim: set colorscheme to sora

--- dot_config/nvim/plugin/10_colorscheme.lua
+++ dot_config/nvim/plugin/10_colorscheme.lua
@@ -1,12 +1,24 @@
-local theme = "vesper"
+local theme = "sora"
 
 vim.pack.add({
   {
-    src = "https://github.com/datsfilipe/vesper.nvim",
+    src = "https://github.com/Aejkatappaja/sora",
     name = theme,
   }
 })
 
-require('vesper').setup {}
+require("sora").setup({
+  transparent = false,
+  italic = true,
+  italic_comments = true,
+
+  on_colors = function(colors)
+    -- colors.bg = "#000000"
+  end,
+
+  on_highlights = function(hl, colors)
+    -- hl.Normal = { fg = colors.fg, bg = "#000000" }
+  end,
+})
 
 vim.cmd.colorscheme(theme)
