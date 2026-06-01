e00b3c1fb261b5c74e45d7b71477593d7fe0ad1d
GeorgiyKuzora <g.kuzora@korona.net>
2026-06-01 18:11:44 +0300

    nvim: return catppuccine

--- dot_config/nvim/plugin/10_colorscheme.lua
+++ dot_config/nvim/plugin/10_colorscheme.lua
@@ -1,24 +1,90 @@
-local theme = "sora"
+local theme = "catppuccin"
 
 vim.pack.add({
   {
-    src = "https://github.com/Aejkatappaja/sora",
+    src = "https://github.com/catppuccin/nvim",
     name = theme,
   }
 })
 
-require("sora").setup({
-  transparent = false,
-  italic = true,
-  italic_comments = true,
-
-  on_colors = function(colors)
-    -- colors.bg = "#000000"
+require(theme).setup {
+  flavour = 'mocha', -- latte, frappe, macchiato, mocha
+  background = { -- :h background
+    light = 'latte',
+    dark = 'mocha',
+  },
+  dim_inactive = {
+    enabled = true, -- dims the background color of inactive window
+    shade = 'dark',
+    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+  },
+  styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { 'italic' }, -- Change the style of comments
+    conditionals = {},
+    loops = {},
+    functions = { 'italic' },
+    keywords = { 'italic' },
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = { 'italic' },
+    operators = {},
+  },
+  lsp_styles = {
+    underlines = {
+        errors = { "undercurl" },
+        hints = { "undercurl" },
+        warnings = { "undercurl" },
+        information = { "undercurl" },
+        ok = { "undercurl" },
+    },
+    inlay_hints = {
+        background = false,
+    },
+  },
+  -- color_overrides = {
+  --   mocha = {
+  --     base = '#181825',
+  --     mantle = '#11111b',
+  --     crust = '#11111b',
+  --   },
+  -- },
+  integrations = {
+    blink_cmp = true,
+    dap = true,
+    dap_ui = true,
+    diffview = true,
+    dropbar = {
+      enabled = true,
+      color_mode = true,
+    },
+    gitsigns = true,
+    harpoon = true,
+    neotest = true,
+    nvim_surround = true,
+    semantic_tokens = true,
+    snacks = true,
+    treesitter = true,
+    treesitter_context = true,
+    which_key = true,
+  },
+  custom_highlights = function(colors)
+    return {
+      DiagnosticLineBackgroundError = { bg = '#2D2331' },
+      DiagnosticLineBackgroundWarn = { bg = '#2D2B32' },
+      DiagnosticLineBackgroundInfo = { bg = '#242B36' },
+      DiagnosticLineBackgroundHint = { bg = '#242B36' },
+      DiagnosticSignError = { bg = '#2D2331' },
+      DiagnosticSignWarn = { bg = '#2D2B32' },
+      DiagnosticSignInfo = { bg = '#242B36' },
+      DiagnosticSignHint = { bg = '#242B36' },
+      SnacksIndentScope = { fg = colors.overlay0 },
+      SnacksIndent = { fg = colors.surface0 },
+      DapStopped = { fg = colors.green },
+    }
   end,
+}
 
-  on_highlights = function(hl, colors)
-    -- hl.Normal = { fg = colors.fg, bg = "#000000" }
-  end,
-})
-
-vim.cmd.colorscheme(theme)
+vim.cmd.colorscheme 'catppuccin-nvim'
