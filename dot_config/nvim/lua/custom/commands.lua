f82f8618bb03ebeb5dcd3cd82eb3ba7bc4c42ebe
GeorgiyKuzora <georgiy@kuzora.ru>
2026-06-03 08:39:44 +0300

    nvim: new commands

--- dot_config/nvim/lua/custom/commands.lua
+++ dot_config/nvim/lua/custom/commands.lua
@@ -154,3 +154,41 @@ end
 vim.api.nvim_create_user_command("BuildLuaSnip", build_lua_snip, {
   desc = 'Build luasnip with make install_jsregexp',
 })
+
+-- PACKAGE MANAGER
+vim.api.nvim_create_user_command("PackAdd", function(opts)
+    vim.pack.add(opts.fargs)
+end, { nargs = "+", desc = "Add plugins (:PackAdd user/repo1 user/repo2)" })
+
+-- Pack Delete and Update cmds are built-in on Nightly 0.13
+vim.api.nvim_create_user_command("PackDel", function(opts)
+    vim.pack.del(opts.fargs)
+end, { nargs = "+", desc = "Delete plugins (:PackDel plugin1 plugin2)" })
+
+vim.api.nvim_create_user_command("PackUpdate", function(opts)
+	-- checks if any argument is passed
+    if opts.args:match("%S") then
+        -- update specific plugins
+        local plugins = vim.split(opts.args, "%s+", { trimempty = true })
+		-- update only specified plugins
+        vim.pack.update(plugins)
+    else
+        -- update all
+        vim.pack.update()
+    end
+end, { nargs = "*", desc = "Update all plugins or specific ones" })
+
+vim.api.nvim_create_user_command("PackCheck", function()
+  local non_active = vim.iter(vim.pack.get())
+    :filter(function(x) return not x.active end)
+    :map(function(x) return x.spec.name .. ".nvim" end)
+    :totable()
+
+  if #non_active == 0 then
+    vim.notify(" No non-active plugins found!", vim.log.levels.INFO)
+    return
+  end
+
+  local message = "Non-active plugins (" .. #non_active .. "):\n" .. table.concat(non_active, "\n")
+  vim.notify(message, vim.log.levels.WARN)
+end, { nargs = "*", desc = "Check non active plugins" })
