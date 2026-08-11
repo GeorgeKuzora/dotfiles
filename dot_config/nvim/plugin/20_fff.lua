c476f5e2face59bb8c5db126430e0a15932a20fa
GeorgiyKuzora <g.kuzora@korona.net>
2026-08-11 08:57:08 +0300

    nvim: fff keybind live_grep_word

--- dot_config/nvim/plugin/20_fff.lua
+++ dot_config/nvim/plugin/20_fff.lua
@@ -26,14 +26,4 @@ require('fff').setup({
 
 vim.keymap.set('n', '<leader><leader>', function() require('fff').find_files() end, { desc = 'FFFind files' })
 vim.keymap.set('n', 'g/', function() require('fff').live_grep() end, { desc = 'FFF Live Grep' })
-vim.keymap.set('n', 'g?', function() require('fff').live_grep({ query = vim.fn.expand("<cword>") }) end, { desc = 'FFF Live Grep' })
-
-Map('x', 'g?', function()
-  local query = GetVisualSelection()
-  require('fff').live_grep({ query = query })
-end, { desc = 'FFF Live Grep (visual selection)' })
-
-Map('x', 'g/', function()
-  local query = GetVisualSelection()
-  require('fff').live_grep({ query = query })
-end, { desc = 'FFF Live Grep (visual selection)' })
+vim.keymap.set({'n', 'x'}, 'g?', function() require('fff').live_grep_under_cursor() end, { desc = 'FFF Live Grep Word' })
